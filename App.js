fcc6cef5b36475014b5720c5d8217037ba6b63d7
paingzin11 <paingzinphyo040@gmail.com>
2023-01-06 13:29:59 +0630

    side_drawer

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './screens/Home';
+import NotificationsScreen from './screens/Notif';
+import AboutScreen  from './screens/About';
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="About" component={AboutScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
